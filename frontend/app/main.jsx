c5353809e01ef2931f57d40a84d55b147314f6cd
Aleksander Grzybowski <kelog@orange.pl>
2016-03-10 08:12:04 +0100

    Working frontend build

---
+++ frontend/app/main.jsx
@@ -0,0 +1,11 @@
+var MainComponent = React.createClass({
+    render: function () {
+        return (
+            <div>
+                React works!
+            </div>
+        )
+    }
+});
+
+ReactDOM.render(<MainComponent/>, document.getElementById('react-target'));
---
+++ frontend/gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+
+var sources = {
+    jsx: [
+        'app/main.jsx'
+    ],
+    libsJs: [
+        'bower_components/react/react-with-addons.js',
+        'bower_components/react/react-dom.js'
+    ],
+    css: [
+        'bower_components/bootstrap/dist/css/bootstrap.css'
+    ],
+    html: [
+        'app/index.html'
+    ]
+};
+
+gulp.task('copy-html', function () {
+    return gulp.src(sources.html)
+        .pipe(gulp.dest('../public/'));
+});
+
+gulp.task('copy-and-concat-css', function () {
+    return gulp.src(sources.css)
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest('../public/'));
+});
+
+gulp.task('copy-and-concat-libs', function () {
+    return gulp.src(sources.libsJs)
+        .pipe(concat('libs.js'))
+        .pipe(gulp.dest('../public/'));
+});
+
+gulp.task('babel', function () {
+    return gulp.src(sources.jsx)
+        .pipe(babel({
+            presets: ['react'],
+            compact: false
+        }))
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('../public'));
+});
+
+gulp.task('default', ['copy-html', 'copy-and-concat-css', 'copy-and-concat-libs', 'babel']);
