271cb476df44332f6089642df75e25b4245dd1d2
Aleksander Grzybowski <kelog@orange.pl>
2016-04-02 18:09:21 +0200

    Fix component scanning

--- src/main/java/org/kelog/todolist/app/Application.java
+++ src/main/java/org/kelog/todolist/app/Application.java
@@ -2,8 +2,10 @@ package org.kelog.todolist.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "org.kelog.todolist")
 public class Application {
     
     public static void main(String[] args) {
