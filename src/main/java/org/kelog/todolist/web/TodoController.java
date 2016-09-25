3f39a092c63f7bbf3ed954dd397e6f5eba91cef7
Aleksander Grzybowski <kelog@orange.pl>
2016-09-25 08:44:51 +0200

    Add stub CORS config

--- src/main/java/org/kelog/todolist/web/TodoController.java
+++ src/main/java/org/kelog/todolist/web/TodoController.java
@@ -14,6 +14,7 @@ import java.util.Collection;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class TodoController {
     
     private final String ROOT = "/todoitems";
