bb938da3119d99aa5600430becef36428c482692
Aleksander Grzybowski <kelog@orange.pl>
2016-09-27 20:00:32 +0200

    Link to refresh page if error happens

--- frontend/src/components/ErrorPage.js
+++ frontend/src/components/ErrorPage.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import { Grid, Row, Col, Alert} from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Grid, Row, Col, Alert } from 'react-bootstrap';
 
-const ErrorPage = () => (
-    <Grid>
-        <Row>
-            <Col md={12}>
-                <Alert bsStyle="danger" style={{marginTop: '50px'}}>
-                    <h4>Server error</h4>
-                    <p>Please refresh the page</p>
-                </Alert>
-            </Col>
-        </Row>
-    </Grid>
-);
+export default class ErrorPage extends Component {
+    refreshBrowser = () => {
+        window.location.reload()
+    };
 
-export default ErrorPage;
+    render() {
+        return (
+            <Grid>
+                <Row>
+                    <Col md={12}>
+                        <Alert bsStyle="danger" style={{marginTop: '50px'}}>
+                            <h4>Server error</h4>
+                            <p>Please <a href="#" onClick={this.refreshBrowser}>refresh</a> the page</p>
+                        </Alert>
+                    </Col>
+                </Row>
+            </Grid>
+        )
+    }
+}
